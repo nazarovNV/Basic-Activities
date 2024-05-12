ba3d12b3f95e047ab3df798b54236b0b74efe1e8
nikolay <nazarov.nikolay15@wb.ru>
2024-05-12 14:52:47 +0300

    Сделал 1 и 2 пункты

---
+++ app/src/main/java/otus/gpb/homework/activities/ActivityB.kt
@@ -0,0 +1,14 @@
+package otus.gpb.homework.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
+    }
+}
